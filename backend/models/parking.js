ab4e6290196fda795c987b850142d1dad6ff0d1f
paksak4 <paksak4@naver.com>
2025-10-28 01:46:36 +0900

    /models/parking.js code 타입 string으로 수정

--- backend/models/parking.js
+++ backend/models/parking.js
@@ -4,7 +4,7 @@ const ParkingSchema = new Schema({
   resourceId: { type: String, required: true, index: true },        // CSV 고유 ID
   name: { type: String, required: true },             // 주차장 이름
   address: { type: String, required: true },          // 주소
-  code: { type: Number, required: true },             // 주차장 코드
+  code: { type: String, required: true },             // 주차장 코드
   lat: { type: Number, required: true },              // 위도
   lon: { type: Number, required: true },              // 경도
   total_parking: { type: Number, default: 0 },        // 총 주차면 수
